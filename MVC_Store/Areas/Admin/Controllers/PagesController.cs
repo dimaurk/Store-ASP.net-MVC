7c13dbaaeb2577a5b12b1c743a7673ea61e2fba4
Dmitry Yurkov <dimaurkov1998@mail.ru>
2019-09-21 12:03:07 +0300

    ASP.Net MVC

---
+++ MVC_Store/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,258 @@
+﻿using MVC_Store.Models.Data;
+using MVC_Store.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Store.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Объявляем список для представления (PageVM)
+            List<PageVM> pageList;
+
+            //Инициализация списка (Db)
+            using (Db db = new Db())
+            {
+                pageList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Возвращаем список в представление
+            return View(pageList);
+        }
+
+        // GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //Проверка модели на валидность
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+
+                //Объявляем переменную для краткого описания (slug)
+                string slug;
+
+                //Инициализируем класс PageDTO
+                PagesDTO dto = new PagesDTO();
+
+                //Присвоим заголовок модели
+                dto.Title = model.Title.ToUpper();
+
+                //Проверяем есть ли краткое описание, если нет то присваиваем его
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                //Убеждаемся, что заголовок и краткое описание уникальны
+                if (db.Pages.Any(x => x.Title == model.Title))
+                {
+                    ModelState.AddModelError("", "That title already exist.");
+                    return View(model);
+                }
+                else if (db.Pages.Any(x => x.Slug == model.Slug))
+                {
+                    ModelState.AddModelError("", "That slug already exist.");
+                    return View(model);
+                }
+
+                //Присваиваем оставшиеся значения модели
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                //Сохраняем модель в базу данных
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            //Передаем сообщение через TempData
+            TempData["SM"] = "You have added a new page!";
+
+            //Переадресуем пользователя на метод Index
+            return RedirectToAction("Index");
+        }
+
+        // GET: Admin/Pages/EditPage
+        [HttpGet]
+        public ActionResult EditPage(int id)
+        {
+            //Объявим модель PageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                //Получаем страницу
+                PagesDTO dto = db.Pages.Find(id);
+
+                //Проверяем, доступна ли страница
+                if (dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+
+                //Инициализируем модель данными
+                model = new PageVM(dto);
+            }
+            //Возвращаем модель в представление
+            return View(model);
+        }
+
+        // POST: Admin/Pages/EditPage
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            //Проверяем модель на валидность
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                //Получаем ID страницы
+                int id = model.Id;
+
+                //Объявляем переменную краткого заголовка
+                string slug = "home";
+
+                //Получвем страницу (по Id)
+                PagesDTO dto = db.Pages.Find(id);
+
+                //Присваиваем название из полученной модели в DTO
+                dto.Title = model.Title;
+
+                //Проверяем краткий заголовок и присваеваем его, если это необходимо
+                if (model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ", "-").ToLower();
+                    }
+                    else
+                    {
+                        slug = model.Slug.Replace(" ", "-").ToLower();
+                    }
+                }
+
+                //Проверяем skug и title на уникальность
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title))
+                {
+                    ModelState.AddModelError("", "That title is already exist.");
+                    return View(model);
+                }
+                else if (db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That slug is already exist.");
+                    return View(model);
+                }
+
+                //Записываем остальные значения в класс DTO
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+
+                //Сохранить изменения в базу
+                db.SaveChanges();
+            }
+            //Устанавливаем сообщение в TimeDate
+            TempData["SM"] = "You have edited the page.";
+
+            //Переадресация пользователя
+            return RedirectToAction("EditPage");
+        }
+
+        // GET: Admin/Pages/PageDetails
+        [HttpGet]
+        public ActionResult PageDetails(int id)
+        {
+            //Объявим модель данных (PaageVM)
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                PagesDTO dto = db.Pages.Find(id);
+
+                //Подьверждаем что страница доступна
+                if (dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+
+                //Присваиваем модели информация из базы
+                model = new PageVM(dto);
+            }
+            //Возвращяем модель в представление
+            return View(model);
+        }
+
+        // GET: Admin/Pages/DeletePage/id
+        [HttpGet]
+        public ActionResult DeletePage(int id)
+        {
+            using (Db db = new Db())
+            {
+                //получаем страницу
+                PagesDTO dto = db.Pages.Find(id);
+
+                //удаляем страницу
+                db.Pages.Remove(dto);
+
+                //сохраняем изменение в базе
+                db.SaveChanges();
+            }
+            //доббавляем сообщение об усппешном удалении
+            TempData["SM"] = "You have deleted a page.";
+
+            //переадресовка пользователя на страницу индекса
+            return RedirectToAction("Index");
+        }
+
+        // GET: Admin/Pages/RecorderPages
+        [HttpPost]
+        public void RecorderPages(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                //Реализуем начальный счетчик
+                int count = 1;
+
+                //Инициализируем модель данных
+                PagesDTO dto;
+
+                //Устанавливаем сортировку для кадлой страницы
+                foreach (var pageId in id)
+                {
+                    dto = db.Pages.Find(pageId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+        }
+    }
+}
---
+++ MVC_Store/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("MVC_Store")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVC_Store")]
+[assembly: AssemblyCopyright("© , 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("b5d87c02-a69c-44cd-9578-0982fd788f9d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
